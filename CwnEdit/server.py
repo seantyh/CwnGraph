c1c18faea7833e7765f2ecb4ca4e286a8610d0bc
seantyh <seantyh@gmail.com>
2019-12-31 17:21:19 +0800

    update server.py

--- CwnEdit/server.py
+++ CwnEdit/server.py
@@ -2,7 +2,7 @@ import sys
 import logging
 from pathlib import Path
 from io import StringIO
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 sys.path.append(str(Path(__file__).parent))
@@ -21,10 +21,17 @@ logger.addHandler(sh)
 
 @app.route("/")
 def check_annot_server():
+    log_buf.truncate(0)
+    log_buf.seek(0)
     ret = update()
-    resp = jsonify(retCode=ret, logs=log_buf.getvalue().split("\n"))
-    return resp
+    log_str = log_buf.getvalue()
+    if ret:
+        return f"<pre style='padding: 1em; border-left: 2px solid blue'>{log_str}</pre>"
+    else:
+        return f"<pre style='padding: 1em; border-left: 2px solid orange'>{log_str}</pre>"        
+    
 
 if __name__ == "__main__":
     app.config['JSON_AS_ASCII'] = False
-    app.run(host="0.0.0.0", port=5200)
+    app.run(host="0.0.0.0", port=5200, debug=True)
+    
