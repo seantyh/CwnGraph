1289337380dcb480e28af9304bc97a3a9cf7cfb7
seantyh <seantyh@gmail.com>
2020-01-06 17:56:41 +0800

    add test codes

--- test/test_consistency.py
+++ test/test_consistency.py
@@ -1,9 +1,28 @@
+
+import logging
+logging.basicConfig()
+logging.getLogger().setLevel("INFO")
+
 import sys
 sys.path.append("CwnEdit")
 from CwnGraph import CwnBase
+import pandas as pd
+# pylint: disable=import-error
 from consistency import check_consistency
 
-def test_consistency():
+from pathlib import Path
+
+
+def test_consistency(caplog):
+    caplog.set_level(logging.INFO)
+    formatter = logging.Formatter("[%(levelname)s] %(name)s: %(message)s")
+    caplog.
     cwn = CwnBase()
-    annot_dfs = {}
+    basepath = Path(__file__).parent / "../data"
+    annot_dfs = {
+        "lemma": pd.read_csv(basepath/"sense_no_relation - lemma.csv"),
+        "sense": pd.read_csv(basepath/"sense_no_relation - sense.csv"),
+        "lex_rel": pd.read_csv(basepath/"sense_no_relation - lexical relation.csv"),
+    }
     check_consistency(cwn, annot_dfs)    
+    assert False
