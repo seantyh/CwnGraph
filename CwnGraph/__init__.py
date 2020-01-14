ce772f467d54df5e0c4e844266a1c1e94beb5430
SeanTYH <seantyh@gmail.com>
2020-01-14 14:06:01 +0800

    work on cwn_checker.py

--- CwnGraph/__init__.py
+++ CwnGraph/__init__.py
@@ -1,5 +1,6 @@
 from .cwn_base import CwnBase
 from .cwn_graph import CWN_Graph
+from .cwn_checker import CwnChecker
 from . import cwnio as io
 from .cwn_graph_utils import CwnGraphUtils
 from .cwn_annotator import CwnAnnotator
--- CwnGraph/cwn_checker.py
+++ CwnGraph/cwn_checker.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from .cwn_graph_utils import CwnGraphUtils
 from .cwn_types import *
 
@@ -7,7 +8,19 @@ class CwnChecker:
         self.suggestions = []
 
     def check_synset_definitions(self):
-        pass
+        def collect_def(old, new):
+            new_id = new[0]
+            new_data = new[1]
+            old.setdefault(new_data["def"], []).append(new_id)
+            return old
+        sense_iter = filter(lambda x: x[1]["node_type"]=="sense", self.cgu.V.items())
+        synonym_maps = reduce(collect_def, sense_iter, {})
+
+        for sense_def, sense_ids in synonym_maps.items():
+            #TODO: to be implemenetedP
+            pass
+            
+        return sense_iter
 
     def check_inverse_relations(self, rel: CwnRelationType, rel_inv: CwnRelationType):
         pass
